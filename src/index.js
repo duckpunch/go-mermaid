35f0c03db43cb4dc7ed7ce7dd1c018e0c1f3c63a
duckpunch <git@duckpunch.org>
2024-06-18 21:25:20 -0700

    More ideas, dev HTML

---
+++ src/index.js
@@ -0,0 +1,76 @@
+import godash from "godash";
+
+console.log("sup");
+console.log(godash);
+
+
+/*
+ * board types:
+ * autoresponse
+ * static - black, white
+ * freeplay - black, white, turn
+ * replay - sequence (pure list)
+ */
+
+// size
+
+// how to figure out corners
+
+/*
+
+type: auto-response
+size: 9
+initBlack: ['af', 'be']
+initWhite: ['ad', 'ae', 'ca', 'cb', 'cc', 'cd']
+tree:
+  bd:
+    response: ac
+    bc:
+      response: ab
+      bb:
+        response: aa
+        ba:
+          result: success
+        zz:
+          response: ba
+          result: fail
+      zz:
+        response: bb
+        result: fail
+    zz:
+      response: bc
+      result: fail
+  zz:
+    response: bd
+    result: fail
+
+alt:
+
+bd, bc, bb, ba
+
+node (user move): response, list[node]
+
+
+bd
+|- ac
+
+
+
+|XX[comment here]
+|XX[another comment]|YY|ZZ
+
+MV[WW];C[comment];X[XX];R[YY]
+| MV[AA];
+
+
+(
+  move: XX
+  comment: [Something interesting]
+  auto: YY
+  response: [
+    (...)
+    (...)
+  ]
+)
+
+ */
